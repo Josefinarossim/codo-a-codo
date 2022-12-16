6ca3259e9abebe39d66d95a5534ec396bc784b59
Josefinarossim <113316894+Josefinarossim@users.noreply.github.com>
2022-12-16 16:05:29 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const valorTicket = 200;
-
-function resumen() {
-    var cantidadTickets=document.getElementById('cantidadTickets').value;
-    var descuento=document.getElementById('categoria').value;
-    var precioTotal = cantidadTickets * ((valorTicket*descuento)/100);
-    document.getElementById("totalCompra").value = "Total a pagar $"+precioTotal
-}
-
